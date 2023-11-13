843a88fe122dd3116e02e72c2cc1a1f7cc5ddb3d
ali829 <alielyassraoui@gmail.com>
2023-11-13 11:17:58 +0100

    project setup + store reducer setup

---
+++ src/store.js
@@ -0,0 +1,34 @@
+const initialStates = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const ACTION = {
+  ACCOUNT_DEPOSIT: "account/deposit",
+  ACCOUNT_WITHDRAW: "account/withdraw",
+  ACCOUNT_REQUEST_LOAN: "account/requestLoan",
+  ACCOUNT_PAY_LOAN: "account/payLoan",
+};
+
+function reducer(state = initialStates, action) {
+  switch (action.type) {
+    case ACTION.ACCOUNT_DEPOSIT:
+      return { ...state, balance: state.balance + action.payload };
+    case ACTION.ACCOUNT_WITHDRAW:
+      return { ...state, balance: state.balance - action.payload };
+    case ACTION.ACCOUNT_REQUEST_LOAN:
+      if (state.loan > 0) return state;
+      //TODO: later
+      return { ...state, loan: action.payload };
+    case ACTION.ACCOUNT_PAY_LOAN:
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
